b77a1450567282a1c4f97bfd01ad3d77951532c6
Danah <osta.danah@gmail.com>
2022-10-16 22:35:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.setAttribute('id','victory');
+
+//document.body.append(newHeader);
+
+newHeader.innerHTML = "Danah is the champion";
+document.body.append(newHeader);
+
+
